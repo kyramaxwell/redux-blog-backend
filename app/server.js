b1586a768a105df686e7fee15a5e9cf2540f9b48
kyramaxwell <kyralmaxwell@gmail.com>
2016-08-09 10:30:34 -0400

    working api

--- app/server.js
+++ app/server.js
@@ -1,6 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import apiRouter from './router';
+
+
+// DB Setup
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/blog';
+mongoose.connect(mongoURI);
+// set mongoose promises to es6 default
+mongoose.Promise = global.Promise;
 
 // initialize
 const app = express();
@@ -12,11 +21,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
-// default index route
-app.get('/', (req, res) => {
-  res.send('hi');
-});
+app.use('/api', apiRouter);
 
 // START THE SERVER
 // =============================================================================
